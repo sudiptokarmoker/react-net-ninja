1ee4927debabc8b9d00896e1e847344443dc5019
sudipto <sudiptocsi@gmail.com>
2021-08-26 18:24:50 +0600

    click method check

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,27 @@
+import { useState } from "react";
+
 const Home = () => {
+    //let name = 'Srijon';
+    const [name, setName] = useState('mario'); // here name is not defined before
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
-        console.log("Handle click fired here");
+        //console.log(name);
+        setName('martin');
+        //console.log(name);
+        setAge(30);
     }
 
-
-    const handClickAgain = (name) => {
-        console.log('Hello ', name);
+    const handClickAgain = (name, e) => {
+        console.log('Hello ', name, e.target);
     }
 
     return (
         <div className="home">
             <h2>homepage test</h2>
+            <p>{name} age is : {age}</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={ () => {
-                handClickAgain('jack');
-            }}>Click me again</button>
+            <button onClick={ (e) => {handClickAgain('jack', e);}}>Click me again</button>
         </div>
     );
 }
