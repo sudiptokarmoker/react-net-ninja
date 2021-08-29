1dfbf6b05a7896e2f9312bacfaa3812d8c76d9be
sudipto <sudiptocsi@gmail.com>
2021-08-29 21:23:23 +0600

    updatd

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogLists from "./BlogLists";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [name, setName] = useState("James");
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -20,17 +21,25 @@ const Home = () => {
   useEffect(() => {
     fetch('http://127.0.0.1:8000/api/blogs')
       .then(res => {
+        if(!res.ok){
+          throw Error("could not fetch the data");
+        }
+        //console.log(res);
         return res.json();
       })
       .then(data => {
         setBlogs(data.data.blogs);
+        setIsLoading(false);
+      })
+      .catch(err => {
+          console.log(err.message);
       });
   }, []);
 
 
   return (
     <div className="home">
-      
+    {isLoading && <div>Loading ....</div>}
     {blogs && <BlogLists blogsListData={blogs} title="Blog post" handleDelete={handleDelete}/>}
       {/* <BlogLists blogsListData={blogs.filter((blog) => blog.author === 'mario')} title="Maris post"/> */}
       <button onClick={() => setName("Linkon")}>Change Name</button>
