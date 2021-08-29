4e285bcea018a13b1a8adf32773b3ac8e088a7a7
sudipto <sudiptocsi@gmail.com>
2021-08-29 21:03:06 +0600

    updated

--- src/Home.js
+++ src/Home.js
@@ -2,37 +2,12 @@ import { useEffect, useState } from "react";
 import BlogLists from "./BlogLists";
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
-    {
-      title: "My new websites",
-      body: "lorel ipsum ....",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Welcome to party",
-      body: "lorel ipsum ....",
-      author: "luige",
-      id: 2,
-    },
-    {
-      title: "My new websites",
-      body: "lorel ipsum ....",
-      author: "adam",
-      id: 3,
-    },
-    {
-      title: "My new websites",
-      body: "lorel ipsum ....",
-      author: "mario",
-      id: 4,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
   const [name, setName] = useState("James");
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlog(newBlogs);
+    setBlogs(newBlogs);
   };
 
   /*
@@ -48,18 +23,15 @@ const Home = () => {
         return res.json();
       })
       .then(data => {
-        console.log(data['data']);
+        setBlogs(data.data.blogs);
       });
   }, []);
 
 
   return (
     <div className="home">
-      <BlogLists
-        blogsListData={blogs}
-        title="Blog post"
-        handleDelete={handleDelete}
-      />
+      
+    {blogs && <BlogLists blogsListData={blogs} title="Blog post" handleDelete={handleDelete}/>}
       {/* <BlogLists blogsListData={blogs.filter((blog) => blog.author === 'mario')} title="Maris post"/> */}
       <button onClick={() => setName("Linkon")}>Change Name</button>
       <p>{name}</p>
