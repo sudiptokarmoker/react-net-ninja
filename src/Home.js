9cae035723ed7f5a41974937f66059cefa04c813
sudiptokarmoker <sudiptocsi@gmail.com>
2021-08-30 21:29:31 +0600

    local changes

--- src/Home.js
+++ src/Home.js
@@ -8,21 +8,35 @@ const Home = () => {
         {title: 'My new websites', body: 'lorel ipsum ....', author: 'adam', id: 3},
         {title: 'My new websites', body: 'lorel ipsum ....', author: 'mario', id: 4}
     ]);
+    const [name, setName] = useState('maria');
 
     const handleDelete  = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlog(newBlogs);
     };
 
+    /*
     useEffect(() => {
         console.log("use effect run here");
         console.log(blogs);
-    })
+    })*/
+    /**
+     * use effect if there is need to run only one first time load: like in jquery
+     * $(document).ready(function(){
+     *  // onload code 
+     * });
+     *  */ 
+    useEffect(() => {
+        console.log("use effect trigger here");
+        console.log(blogs);
+    }, [name]); // this name array is dependency hook. it will just trigger if this matches on staging. value change or anything else.
 
     return (
         <div className="home">
             <BlogLists blogsListData={blogs} title="Blog post" handleDelete={handleDelete}/>
             {/* <BlogLists blogsListData={blogs.filter((blog) => blog.author === 'mario')} title="Maris post"/> */}
+            <button onClick={() => setName('jag hira')}>Change name</button>
+            <p>{name}</p>
         </div>
     );
 }
