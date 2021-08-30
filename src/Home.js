caf1934e2459d23eb863351fe10b8c29b6793df8
sudiptokarmoker <sudiptocsi@gmail.com>
2021-08-30 21:32:44 +0600

    latest

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,11 @@ import { useEffect, useState } from "react";
 import BlogLists from "./BlogLists";
 
 const Home = () => {
-    const [blogs, setBlog] = useState([
-        {title: 'My new websites', body: 'lorel ipsum ....', author: 'mario', id: 1},
-        {title: 'Welcome to party', body: 'lorel ipsum ....', author: 'luige', id: 2},
-        {title: 'My new websites', body: 'lorel ipsum ....', author: 'adam', id: 3},
-        {title: 'My new websites', body: 'lorel ipsum ....', author: 'mario', id: 4}
-    ]);
+    const [blogs, setBlog] = useState(null);
     const [name, setName] = useState('maria');
-  const [blogs, setBlogs] = useState(null);
-  const [name, setName] = useState("James");
-  const [isLoading, setIsLoading] = useState(true);
+    const [blogs, setBlogs] = useState(null);
+    const [name, setName] = useState("James");
+    const [isLoading, setIsLoading] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
